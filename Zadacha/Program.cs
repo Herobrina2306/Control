a58e8f008765516c550609ced2bbda98a3e48545
AleksandraM <musatova.aleksandra@mail.ru>
2022-10-16 17:53:53 +0300

    Создала метод и проверила

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -2,14 +2,34 @@
 
 string[] array = {"Привет.", "Как", "дела", "?", "Вы", "разрешили", "ввести", "массив", "самой.", ";)"};
 
+string[] newArray = Selection(array);
+
 PrintArray(array);
+Console.Write(" -> ");
+PrintArray(newArray);
 
-// string[] Selection (string[] arr)
-// {
+string[] Selection(string[] arr)
+{
+    int size = arr.Length;
+    string[] arrayFin = new string[size];
+    int len = 3;
+    int NumberOfCharacters = 0;
+    int counterArrayFin = 0;
+    for(int i = 0; i< arr.Length; i++)
+    {
+        NumberOfCharacters = arr[i].Length;
+        if(NumberOfCharacters <= len)
+        {
+            arrayFin[counterArrayFin] = arr[i];
+            counterArrayFin++;
+        }
+        
+    }
+    
 
 
-
-// }
+return arrayFin;
+}
 
 void PrintArray(string[] arr)
 {
