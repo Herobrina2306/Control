9d70f6d86f7df28a51d1bf7011a1acf45e79ca87
AleksandraM <musatova.aleksandra@mail.ru>
2022-10-16 17:33:14 +0300

    Создала массив и сделала его вывод на консоль

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа выводит эленеты массива в котором три или меньше символа.
+
+string[] array = {"Привет.", "Как", "дела", "?", "Вы", "разрешили", "ввести", "массив", "самой.", ";)"};
+
+PrintArray(array);
+
+// string[] Selection (string[] arr)
+// {
+
+
+
+// }
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write($"{arr[arr.Length-1]}]");
+}
